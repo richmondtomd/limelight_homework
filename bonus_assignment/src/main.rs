e3f25d49abc0d53b0b525e74026923aa29c9f6f2
richmondtomd <richmondtomd@gmail.com>
2022-04-17 18:55:58 -0500

    bonus assignment threading

--- bonus_assignment/src/main.rs
+++ bonus_assignment/src/main.rs
@@ -1,9 +1,13 @@
 use std::collections::HashMap;
+use std::sync::{Mutex, Arc};
+use std::thread;
 
 use domain_info::{process, models};
 
 fn main() -> Result<(), String> {
-    let mut result: HashMap<&str, models::ResponseInfo> = HashMap::new();
+    // let mut result: HashMap<&str, models::ResponseInfo> = HashMap::new();
+    let result = Arc::new(Mutex::new(HashMap::new()));
+    let mut handles = vec![];
 
     let url = "https://adrien-kiren-f22c-layer0-tech-test-default.layer0-limelight.link";
     let mut response =  ureq::get(url).call();
@@ -12,16 +16,32 @@ fn main() -> Result<(), String> {
     while response.is_err() {
         response =  ureq::get(url).call();
     }
+
+    //Get Response body
     let response = response.unwrap();
     let response_body = response.into_string().unwrap();
 
-    let list_domains: Vec<&str> = serde_json::from_str(&response_body).unwrap();
+    //Retrieve list of domains from response body
+    let list_domains: Vec<String> = serde_json::from_str(&response_body).unwrap();
 
+    //Loop through domains
     for domain in list_domains {
-        result.insert(domain, process::process(domain)?);
+        let inner_result = Arc::clone(&result);
+        //Spawn thread to process domain
+        let handle = thread::spawn(move || {
+            let domain_info = process::process(domain.as_ref()).unwrap();
+            inner_result.lock().unwrap().insert(domain, domain_info);
+        });
+        handles.push(handle);
     }
 
-    println!("{}", serde_json::to_string_pretty(&result).unwrap());
+    //Join Handles
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    //Pretty print results in json format
+    println!("{}", serde_json::to_string_pretty(&*result.lock().unwrap()).unwrap());
 
     Ok(())
 }
